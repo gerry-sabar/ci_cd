ab06fc6713fd677e1e71eeb21e035ce396d72c23
Gerry <gerrysabar@gmail.com>
2020-04-20 21:04:51 +0700

    first commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        //uncomment code below for fail test on AWS Pipeline
+        //$this->fail('Test force failure');
+        $this->assertTrue(true);
+    }
+}
